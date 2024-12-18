8ae4698c2da1e554a0b944279b70eca7297232cf
warrioralok <34855411+warrioralok@users.noreply.github.com>
2024-12-18 18:14:16 +0530

    provider updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import Dashboard from "./Component/Dashboard";
 import "./App.css";
 import { NavBar } from "./Component/NavBar";
-import store from "./Action";
-import { Provider } from "react-redux";
 const App = () => {
   return (
     <>
       <NavBar />
-      <Provider store={store}>
-        <Dashboard />
-      </Provider>
+      <Dashboard />
     </>
   );
 };
